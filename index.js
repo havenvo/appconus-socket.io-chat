de2eaed451196ddb1f1cffe035c8fd9c908453e3
havenvo <vthieu09t4@gmail.com>
2017-04-01 10:37:46 +0700

    Socket IO redis adapter for running multiple node

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
+var redis = require('socket.io-redis');
+io.adapter(redis({ host: 'localhost', port: 6379 }));
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
